0e80a79779b3cf7ac017f54e69db943634f8dcbd
mehmet1508 <mehmet.cay2@ogr.sakarya.edu.tr>
2023-10-25 18:10:49 +0300

    dosyalar eklendi

---
+++ kerem3.cpp/kerem3.cpp/kerem3.cpp.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	double bacteria;
+	int h;
+	bacteria = 1000;
+	for (h = 0;h <= 24;h++) {
+		bacteria = bacteria + (bacteria * 150 / 100);
+		cout << bacteria << endl;
+
+
+			
+
+	}
+}
