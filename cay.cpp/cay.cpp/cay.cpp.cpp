bd66175cf0fadc50cda8ba77967bcb4e1790bfba
mehmet1508 <mehmet.cay2@ogr.sakarya.edu.tr>
2023-10-25 17:50:54 +0300

    tüm dosyalar girildi

---
+++ cay.cpp/cay.cpp/cay.cpp.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int puan;
+	cout << "0-100 arası bir puan giriniz ";
+	cin >> puan;
+	if (puan >= 85 && puan <= 100) {
+		cout << "5";
+	}
+	else if (puan >= 70 && puan <= 84) {
+		cout << "4";
+	}
+	else if (puan >= 55 && puan <= 69) {
+		cout << "3";
+	}
+	else if (puan >= 45 && puan <= 54) {
+		cout << "2";
+	}
+	else if (puan >= 0 && puan <= 44) {
+		cout << "1";
+	}
+	else {
+		cout << "böyle bir puan yok";  
+	}
+	
+}
